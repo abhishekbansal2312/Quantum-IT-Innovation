300242f4ed796c3a3eeb9f782d0c9029ac779cb3
Abhishek Bansal <abhishekbansal2312@gmail.com>
2025-04-03 16:24:37 +0530

    Add authentication routes and controller

---
+++ server/routes/auth.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const router = express.Router();
+const { check } = require("express-validator");
+const authController = require("../controllers/authController");
+const auth = require("../middleware/auth");
+
+router.post(
+  "/register",
+  [
+    check("name", "Name is required").not().isEmpty(),
+    check("email", "Please include a valid email").isEmail(),
+    check("password", "Password must be at least 6 characters").isLength({
+      min: 6,
+    }),
+    check("dateOfBirth", "Date of birth is required").not().isEmpty(),
+  ],
+  authController.register
+);
+
+router.post(
+  "/login",
+  [
+    check("email", "Please include a valid email").isEmail(),
+    check("password", "Password is required").exists(),
+  ],
+  authController.login
+);
+
+router.get("/user", auth, authController.getCurrentUser);
+
+module.exports = router;
